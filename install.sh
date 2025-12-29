7dde2bd9ad203f64f943ec8dc25e06dc3c3726aa
dev-sda1 <43112896+dev-sda1@users.noreply.github.com>
2025-12-29 01:00:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ chmod +x /home/${SUDO_USER}/.local/bin/rofi-power-menu
 echo -e "\nNow configuring ZSH.."
 dnf install -y zsh
 chsh $SUDO_USER -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || echo "oh-my-zsh already installed.."
 
 echo -e "\n Copying config files"
 
