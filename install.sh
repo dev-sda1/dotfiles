7e05cfefcf69a56c304cb005873f4114281e8291
dev-sda1 <43112896+dev-sda1@users.noreply.github.com>
2025-12-29 01:25:03 +0000

    small adjust

--- install.sh
+++ install.sh
@@ -4,67 +4,67 @@ set -e
 ## This acts as a one-stop setup file for dotfiles, and certain software.
 ## Verified working with Fedora 43.
 
-if [ $(id -u) -ne 0 ]
-    then echo "This should be run as sudo."
-    exit -1
-fi
-
 ## Full system update first..
-dnf update -y
+sudo dnf update -y
 
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
-dnf install akmod-nvidia -y
-dnf install xorg-x11-drv-nvidia-cuda -y
+sudo dnf install akmod-nvidia -y
+sudo dnf install xorg-x11-drv-nvidia-cuda -y
 
-dnf group install development-tools -y
-dnf install git -y
+sudo dnf group install development-tools -y
+sudo dnf install git -y
 
 ## Enabling hyprland COPR repository
-dnf copr enable solopasha/hyprland -y
-dnf copr enable scottames/ghostty -y
+sudo dnf copr enable solopasha/hyprland -y
+sudo dnf copr enable scottames/ghostty -y
 
 ## VSCode keys
 rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 # Packages
 
-dnf install python3 -y
-dnf install pip3 -y
-dnf install -y pavucontrol
-dnf install code -y
-dnf install hyprland -y
-dnf install hyprpaper -y
-dnf install hyprlock -y
-dnf install ghostty -y
-dnf install neovim -y
-dnf install rofi -y
-dnf install waybar -y
-dnf install gammastep -y
-dnf install slurp -y
-dnf install jetbrains-mono-fonts-all -y
-dnf install curl -y
+sudo dnf install python3 -y
+sudo dnf install pip3 -y
+sudo dnf install -y pavucontrol
+sudo dnf install code -y
+sudo dnf install hyprland -y
+sudo dnf install hyprland-qtutils -y
+sudo dnf install hyprpaper -y
+sudo dnf install hyprlock -y
+sudo dnf install ghostty -y
+sudo dnf install neovim -y
+sudo dnf install rofi -y
+sudo dnf install waybar -y
+sudo dnf install gammastep -y
+sudo dnf install slurp -y
+sudo dnf install jetbrains-mono-fonts-all -y
+sudo dnf install curl -y
 
 # Other packages
-dnf install steam -y
-dnf install telegram-desktop -y
+sudo dnf install steam -y
+sudo dnf install telegram-desktop -y
 
-mkdir -p /home/${SUDO_USER}/.local/bin/
-wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/refs/heads/master/rofi-power-menu -O /home/${SUDO_USER}/.local/bin/rofi-power-menu
-chmod +x /home/${SUDO_USER}/.local/bin/rofi-power-menu
+mkdir -p /home/${USER}/.local/bin/
+wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/refs/heads/master/rofi-power-menu -O /home/${USER}/.local/bin/rofi-power-menu
+chmod +x /home/${USER}/.local/bin/rofi-power-menu
 
 echo -e "\nNow configuring ZSH.."
-dnf install -y zsh
-chsh $SUDO_USER -s /usr/bin/zsh
+sudo dnf install -y zsh
+chsh $USER -s /usr/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || echo "oh-my-zsh already installed.."
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' /home/${USER}/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo -e "\n Copying config files"
 
-mkdir -p /home/${SUDO_USER}/.config
-cp -r ${PWD}/ghostty /home/${SUDO_USER}/.config/
-cp -r ${PWD}/hypr /home/${SUDO_USER}/.config/
-cp -r ${PWD}/nvim /home/${SUDO_USER}/.config/
-cp -r ${PWD}/rofi /home/${SUDO_USER}/.config/
-cp -r ${PWD}/waybar /home/${SUDO_USER}/.config/
+mkdir -p /home/${USER}/.config
+cp -r ${PWD}/ghostty /home/${USER}/.config/
+cp -r ${PWD}/hypr /home/${USER}/.config/
+cp -r ${PWD}/nvim /home/${USER}/.config/
+cp -r ${PWD}/rofi /home/${USER}/.config/
+cp -r ${PWD}/waybar /home/${USER}/.config/
 
 echo -e "\n Fully complete! Restart system to finish."
